eb2b0a4b75519b8d42230d189ef51c0a188d0aed
yangkai <ykredhat@163.com>
2016-12-11 17:06:57 +0800

    lab205修改

--- src/helloworld.java
+++ src/helloworld.java
@@ -5,7 +5,7 @@
  */
 public class helloworld {
     public static void main(String[] args){
-        System.out.println("sdfsdfsdf");
+        System.out.println("sdfsdfsjdsfdfsdffgvfdgkhkxcxcdf");
     }
 }
 
